2fa9016acb7982c7ddb3ae9a7f854e3b6e0ea089
Codequeen <54287358+Estherokafor05@users.noreply.github.com>
2024-09-20 15:07:56 +0100

    remove expect

--- test/test.spec.js
+++ test/test.spec.js
@@ -1,4 +1,4 @@
-const { test, expect } = require("@playwright/test");
+const { test } = require("@playwright/test");
 const { createObjectCsvWriter } = require("csv-writer");
 
 test("Extract Stack Overflow Questions", async ({ page }) => {
